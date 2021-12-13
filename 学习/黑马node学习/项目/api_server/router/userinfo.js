1b30aae15095d110cf32aca708f6fb8b4dabf510
黄文杰 <1606354739@qq.com>
2021-12-14 00:20:34 +0800

    更新

--- 学习/黑马node学习/项目/api_server/router/userinfo.js
+++ 学习/黑马node学习/项目/api_server/router/userinfo.js
@@ -2,11 +2,10 @@ const express = require("express");
 
 // 创建路由对象
 const router = express.Router();
-
+//导入路由处理模块
+const userinfo_handler = require("../router_handler/userinfo");
 //获取用户基本信息路由
-router.get("/userinfo", (req, res) => {
-  res.send("ok");
-});
+router.get("/userinfo", userinfo_handler.getUserInfo);
 
 // 将路由对象共享出去
 module.exports = router;
