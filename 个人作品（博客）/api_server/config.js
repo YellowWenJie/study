a3486e4cac6fa1a075313c046cc33cb87c8b4741
黄文杰 <1606354739@qq.com>
2021-12-15 17:37:47 +0800

    整理文件夹

---
+++ 个人作品（博客）/api_server/config.js
@@ -0,0 +1,7 @@
+//这是个全局的配置对象
+module.exports = {
+  //加密和解密的token的秘钥
+  jwtSecretKey: "huangwenjie",
+  //token的有效期
+  expiresIn: "10h"
+};
---
+++ 个人作品（博客）/api_server/router/user.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+// 创建路由对象
+const router = express.Router();
+
+// 导入用户路由处理函数模块
+const userHandler = require("../router_handler/user");
+
+// 导入验证表单数据的中间件
+const expressJoi = require("@escook/express-joi");
+
+// 导入需要的验证规则对象
+const { res_login_schema } = require("../schema/user");
+
+// 注册新用户
+router.post("/reguser", expressJoi(res_login_schema), userHandler.regUser);
+
+// 登录
+router.post("/login", expressJoi(res_login_schema), userHandler.login);
+
+// 将路由对象共享出去
+module.exports = router;
