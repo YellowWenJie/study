26f5a09987240c864bb8945b29c83e6a80d892e7
YellowWenJie <89182679+YellowWenJie@users.noreply.github.com>
2021-12-25 22:36:02 +0800

    更新

--- vue/尚硅谷/test/Vue/src/components/HelloWorld.vue
+++ vue/尚硅谷/test/Vue/src/components/HelloWorld.vue
@@ -1,112 +1,61 @@
 <template>
-  <div class="hello">
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <HyHeader :addTodos="addTodo" />
-        <MyList :todos="tools" :checkTodo="checkTodo" :deleteTodo="deleteTodo" />
-        <MyFooter :tools="tools" :checkAllTodo="checkAllTodo" :clearAllTodo="clearAllTodo" />
-      </div>
-    </div>
+  <div>
+    <h1>{{sum}}</h1>
+    <h1>{{bigSum}}</h1>
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="increment(n)">+</button>
+    <button @click="decrement(n)">-</button>
+    <button @click="incrementOdd">偶数加</button>
+    <button @click="incrementWait(n)">过一会加</button>
   </div>
 </template>
 
 <script>
-import HyHeader from "./MyHeader.vue";
-import MyList from "./MyList.vue";
-import MyFooter from "./MyFooter.vue";
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
 export default {
-  name: "HelloWorld",
-  components: { HyHeader, MyList, MyFooter },
   data() {
     return {
-      tools: [
-        { id: "001", title: "黄文杰", done: true },
-        { id: "002", title: "彭于晏", done: false },
-        { id: "003", title: "黄礼志", done: true },
-      ],
+      n: 1,
     };
   },
   methods: {
-    //增
-    addTodo(todoObj) {
-      this.tools.unshift(todoObj);
+    // increment() {
+    //   this.$store.commit("ADD", this.n);
+    // },
+    // decrement() {
+    //   this.$store.commit("DECREMENT", this.n);
+    // },
+    ...mapMutations("a", {
+      increment: "ADD",
+      decrement: "DECREMENT",
+    }),
+    incrementOdd() {
+      this.$store.dispatch("a/Odd", this.n);
     },
-    //勾选与取消勾选
-    checkTodo(id) {
-      this.tools.forEach((todo) => {
-        if (todo.id === id) todo.done = !todo.done;
-      });
-    },
-    //删除一个todo
-    deleteTodo(id) {
-      this.tools = this.tools.filter((todo) => {
-        return todo.id !== id;
-      });
-    },
-    //全选与取消全选
-    checkAllTodo(done) {
-      this.tools.forEach((todo) => {
-        todo.done = done;
-      });
-    },
-    //清除所有已经完成的todo
-    clearAllTodo() {
-      this.tools = this.tools.filter((todo) => {
-        return !todo.done;
-      });
-    },
+    // incrementWait() {
+    //   this.$store.dispatch("addWait", this.n);
+    // },
+    ...mapActions("a", {
+      // incrementOdd: "Odd",
+      incrementWait: "addWait",
+    }),
   },
-  watch: {
-    tools(value) {
-      localStorage.setItem("tools", JSON.stringify(value));
-    },
+  computed: {
+    // sum() {
+    //   return this.$store.state.sum;
+    // },
+    ...mapState("a", ["sum"]),
+    // bigSum() {
+    //   return this.$store.getters.bigSum;
+    // },
+    ...mapGetters("a", ["bigSum"]),
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style >
-/*base*/
-body {
-  background: #fff;
-}
-
-.btn {
-  display: inline-block;
-  padding: 4px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
-  line-height: 20px;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
-    0 1px 2px rgba(0, 0, 0, 0.05);
-  border-radius: 4px;
-}
-
-.btn-danger {
-  color: #fff;
-  background-color: #da4f49;
-  border: 1px solid #bd362f;
-}
-
-.btn-danger:hover {
-  color: #fff;
-  background-color: #bd362f;
-}
-
-.btn:focus {
-  outline: none;
-}
-
-.todo-container {
-  width: 600px;
-  margin: 0 auto;
-}
-.todo-container .todo-wrap {
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
+<style>
 </style>
